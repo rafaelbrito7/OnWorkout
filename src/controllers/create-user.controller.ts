4bffa2392558d187a8d3eee86477634990653ae4
Rafael Brito <rafael.souzabrito7@gmail.com>
2024-03-22 21:27:32 -0300

    feat: pipe de validação de schema do body

--- src/controllers/create-user.controller.ts
+++ src/controllers/create-user.controller.ts
@@ -4,9 +4,19 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { hash } from 'bcryptjs'
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const createUserBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type CreateUserBodySchema = z.infer<typeof createUserBodySchema>
 
 @Controller('/users')
 export class CreateUserController {
@@ -14,7 +24,8 @@ export class CreateUserController {
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createUserBodySchema))
+  async handle(@Body() body: CreateUserBodySchema) {
     const { email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
